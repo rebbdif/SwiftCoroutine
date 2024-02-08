889024d1ff08e0205346b3e0ce6f69b63241251f
Leonid Serebryanyy <leosilver@yandex.ru>
2024-02-08 18:34:50 +0200

    Update Coroutine+StackSize.swift

--- Sources/SwiftCoroutine/Coroutine/Coroutine/Coroutine+StackSize.swift
+++ Sources/SwiftCoroutine/Coroutine/Coroutine/Coroutine+StackSize.swift
@@ -16,7 +16,7 @@ extension Coroutine {
 
 extension Coroutine.StackSize {
     
-    internal static let recommended = Coroutine.StackSize(size: 192 * 1024)
+    internal static let recommended = Coroutine.StackSize(size: 512 * 1024)
     
     internal static func pages(_ number: Int) -> Coroutine.StackSize {
         .init(size: number * .pageSize)
